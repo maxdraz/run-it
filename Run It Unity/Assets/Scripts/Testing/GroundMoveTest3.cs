689541695e34a2819531260acc3fa3a9fc3f0305
maxdraz <maksdrazew@gmail.com>
2021-02-09 21:01:11 +0000

    More ground move polish

--- Run It Unity/Assets/Scripts/Testing/GroundMoveTest3.cs
+++ Run It Unity/Assets/Scripts/Testing/GroundMoveTest3.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using RunIt.Console;
 using RunIt.Detection;
 using RunIt.Movement;
+using RunIt.Rotation;
 using UnityEngine;
 using UnityEngine.InputSystem.Composites;
 using UnityEngine.UI;
@@ -9,38 +11,25 @@ namespace RunIt.Testing
 {
     public class GroundMoveTest3 : ParkourBehaviour
     {
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float maxAcceleration;
+        [SerializeField] private float friction;
+        private Vector3 inputDir;
         private Vector3 acceleration;
-        private float maxSpeed;
-        private float maxAcceleration;
-
         private Vector3 velocity;
+   
         private void Update()
         {
-            var inputDir = GetLocalInputDirection();
+            ConsoleCommands.ClearConsole();
 
-            acceleration = CalculateAcceleration(inputDir, maxSpeed, maxAcceleration);
-
-            velocity += acceleration * Time.deltaTime;
-            velocity.y = rb.velocity.y;
+            inputDir = GetLocalInputDirection();
             
+            var prevVel = velocity;
+            velocity = MoveGround(inputDir, prevVel);
+            velocity.y = rb.velocity.y;
             rb.velocity = velocity;
         }
 
-        private Vector3 CalculateAcceleration(Vector3 direction, float desiredSpeed, float accel )
-        {
-            
-
-            var currentSpeed = Vector3.Dot(velocity, direction);
-            var addSpeed = desiredSpeed - currentSpeed;
-
-            if (addSpeed <= 0)  // going where we want at the speed we want
-            {
-                return direction * accel;
-            }
-            
-            return Vector3.zero;
-        }
-        
         private Vector3 GetLocalInputDirection()
         {
             var inputRaw = action.ReadValue<Vector2>();
@@ -49,5 +38,44 @@ namespace RunIt.Testing
             
             return transform.TransformDirection(dir);
         }
+
+        private Vector3 CalculateAcceleration(Vector3 currentVelocity,Vector3 accelDir,float accel, float maxVel)
+        {
+            var projVel = Vector3.Dot(currentVelocity, accelDir);
+            var accelVel = accel;
+            if (projVel + accel > maxVel)
+            {
+                accelVel = maxSpeed - projVel;
+            }
+            
+            return currentVelocity + accelDir * accelVel;
+        }
+        
+        private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVelocity, float accelerate, float max_velocity)
+        {
+            float projVel = Vector3.Dot(prevVelocity, accelDir); // Vector projection of Current velocity onto accelDir.
+            float accelVel = accelerate * Time.deltaTime; // Accelerated velocity in direction of movment
+
+            // If necessary, truncate the accelerated velocity so the vector projection does not exceed max_velocity
+            if(projVel + accelVel > max_velocity)
+                accelVel = max_velocity - projVel;
+
+            return prevVelocity + accelDir * accelVel;
+        }
+
+        private Vector3 MoveGround(Vector3 accelDir, Vector3 prevVelocity)
+        {
+            // Apply Friction
+            float speed = prevVelocity.magnitude;
+            if (speed != 0) // To avoid divide by zero errors
+            {
+                float drop = speed * friction * Time.deltaTime;
+                prevVelocity *= Mathf.Max(speed - drop, 0) / speed; // Scale the velocity based on friction.
+            }
+
+            // ground_accelerate and max_velocity_ground are server-defined movement variables
+            return Accelerate(accelDir, prevVelocity, maxAcceleration, maxSpeed);
+        }
+
     }
 }
---
+++ Run It Unity/Assets/Scripts/Testing/GroundMoveTest4.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using RunIt.Console;
+using RunIt.Detection;
+using RunIt.Movement;
+using RunIt.Rotation;
+using UnityEngine;
+
+namespace RunIt.Testing
+{
+    public class GroundMoveTest4 : ParkourBehaviour
+    {
+        [SerializeField] private float maxSidewaysSpeed;
+        [SerializeField] private float maxForwardSpeed;
+        [SerializeField] private float sidewaysAcceleration;
+        [SerializeField] private float forwardAcceleration;
+        [SerializeField] private float turnDelta;
+        [SerializeField] private float strength;
+        private float maxAcceleration;
+        private float maxSpeed;
+        [SerializeField] private float turnDeltaThreshold;
+        [SerializeField] private float groundFriction = 10f;
+        [SerializeField] private float turnFriction = 10f;
+        [SerializeField] private Vector3 velocity;
+        [SerializeField] private Vector3 acceleration;
+        [SerializeField] private InputRotator rotator;
+        [SerializeField] private Detector groundDetector;
+
+        private void OnEnable()
+        {
+            groundDetector.Detected += OnGrounded;
+        }
+        private void OnDisable()
+        {
+            groundDetector.Detected -= OnGrounded;
+        }
+
+        private void Update()
+        {
+          //  ConsoleCommands.ClearConsole();
+          if(!groundDetector.detected) return;
+            var inputDir = GetLocalInputDirection();
+            turnDelta = rotator.AngleDelta;
+            
+            var dotInputForward = Vector3.Dot(inputDir, transform.forward);
+            if (dotInputForward <= 0.7f)    // if moving to sides or backwards
+            {
+                maxSpeed = maxSidewaysSpeed;
+                maxAcceleration = sidewaysAcceleration;
+            }
+            else
+            {
+                maxSpeed = maxForwardSpeed;
+                maxAcceleration = forwardAcceleration;
+            }
+            
+            if (inputDir == Vector3.zero)   // apply friction when no input
+            {
+                acceleration = -velocity * groundFriction;
+                if (velocity.magnitude <= 0.01f)
+                {
+                    velocity = Vector3.zero;
+                }
+            }
+            else // otherwise accelerate normally
+            {
+                acceleration = inputDir * maxAcceleration;
+            } 
+            
+            if (turnDelta >= turnDeltaThreshold) //if turning rapidly
+            {
+                // maxSpeed = slowSpeed;   //slow down the character
+                acceleration = -velocity * turnFriction;
+                print("clamping");
+            }
+
+            if (dotInputForward >= 0.7f)
+            {
+                var desired = inputDir * maxSpeed;
+                var current = velocity;
+                var targetVel = desired - current;
+                acceleration = velocity + targetVel * strength;
+            }
+                
+                
+            velocity += acceleration * Time.deltaTime;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity.y = rb.velocity.y;
+            rb.velocity = velocity;
+            
+//            print("turn delta: " + turnDelta);
+        }
+        
+        private Vector3 GetLocalInputDirection()
+        {
+            velocity = rb.velocity;
+            
+            var inputRaw = action.ReadValue<Vector2>();
+            var dir = new Vector3(inputRaw.x, 0,
+                inputRaw.y).normalized;
+            return transform.TransformDirection(dir);
+        }
+
+        void OnGrounded()
+        {
+            velocity = transform.forward * maxSpeed;
+        }
+    }
+}
