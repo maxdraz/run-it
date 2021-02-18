9182dc9724c82b62e7471806ac1a51ec2f33e4d9
maxdraz <maksdrazew@gmail.com>
2021-02-18 19:31:28 +0000

    More work on leaderboard

--- Run It Unity/Assets/Scripts/UI/Leaderboard/LeaderboardManager.cs
+++ Run It Unity/Assets/Scripts/UI/Leaderboard/LeaderboardManager.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using RunIt.Detection;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace RunIt.UI.Leaderboard
 {
+    public class LeaderboardItemData
+    {
+        public float time;
+        public string playerName;
+    }
+    
     public class LeaderboardManager : MonoBehaviour
     {
         [SerializeField] private Transform leaderboardItemParent;
@@ -13,6 +21,7 @@ namespace RunIt.UI.Leaderboard
         [SerializeField] private int numEntries = 1;
         [SerializeField] private float yOffset = 20;
         [SerializeField] private Detector displayLeaderboardDetector;
+        private List<LeaderboardItemData> leaderboard;
 
         private List<LeaderboardItem> entries;
 
@@ -30,7 +39,8 @@ namespace RunIt.UI.Leaderboard
 
         private void Start()
         {
-            GenerateLeaderboard();
+            SetChildrenActive(false);
+            TestJson();
         }
 
         void OnEnableLeaderboard(Collider other)
@@ -42,10 +52,17 @@ namespace RunIt.UI.Leaderboard
         void OnDisableLeaderboard(Collider other)
         {
             SetChildrenActive(false);
+
+            for (int i = 0; i < leaderboardItemParent.childCount; i++)
+            {
+                var child = leaderboardItemParent.GetChild(i);
+                Destroy(child.gameObject);
+            }
         }
         
         private void GenerateLeaderboard()
         {
+            
             for (int i = 0; i < numEntries; i++)
             {
                 var item = GameObject.Instantiate(leaderboardItemPrefab, leaderboardItemParent);
@@ -67,5 +84,46 @@ namespace RunIt.UI.Leaderboard
                 transform.GetChild(i).gameObject.SetActive(value);
             }
         }
+
+        static int SortByTime(LeaderboardItemData a, LeaderboardItemData b)
+        {
+            return a.time.CompareTo(b.time);
+        }
+
+        private List<LeaderboardItemData> TestSorting()
+        {
+            leaderboard = new List<LeaderboardItemData>();
+            //add random items
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new LeaderboardItemData() {playerName = Utilities.Utilities.RandomName(5), time = Random.Range(10,5000)};
+                leaderboard.Add(item);
+            }
+            leaderboard.Sort(SortByTime);
+
+            return leaderboard;
+        }
+
+        private void TestJson()
+        {
+            var items = TestSorting();
+            string json = "";
+            foreach (var item in items)
+            {
+                json += JsonUtility.ToJson(item);
+            }
+            
+            File.WriteAllText(Application.dataPath+"/savefile.json", json);
+            
+            //json = JsonUtility.ToJson
+           
+        }
+
+        //private LeaderboardItemData loadLeaderboardItemData()
+        //{
+            //JsonUtility.FromJson<LeaderboardItemData>()
+           // string json = File.ReadAllText(Application.dataPath + "/SaveData/LeaderboardData.json");
+            //File.
+        //}
     }
 }
