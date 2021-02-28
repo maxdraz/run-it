c3d88bb815ff56efc052d4c752f4b15177cc6b74
maxdraz <maksdrazew@gmail.com>
2021-02-28 12:40:43 +0000

    Finished tutorial level structure

---
+++ Run It Unity/Assets/Scripts/Enemies/Lava.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RunIt.Player;
+using UnityEngine;
+
+namespace RunIt.Enemies
+{
+    public class Lava : Damageable
+    {
+        private bool dealtDamage;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (dealtDamage) return;
+            
+            var health = other.gameObject.GetComponent<Health>();
+            if (!health) return;
+            
+            health.SubtractHealth(damage);
+          
+            dealtDamage = true;
+        }
+
+    }
+}
